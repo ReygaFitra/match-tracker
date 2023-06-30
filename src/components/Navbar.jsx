14f573cc2e7a0149680bec6aa32a7ac6e59a8317
ReygaFitra <regafitra12@gmail.com>
2023-06-30 08:36:58 +0700

    styles: main page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <Flex as={'nav'} p="10px" alignItems="center" justifyContent="space-evenly" shadow="sm">
-      <Heading size="md">
+      <Heading size="md" color="#6527BE">
         <Link href="/">Home</Link>
       </Heading>
       <HStack>
-        <Button size="sm" variant="ghost" rounded="sm">
+        <Button size="sm" variant="ghost" rounded="sm" _hover={{ bgColor: '#9681EB', color: 'white', rounded: 'sm' }}>
           <Link href="/club">Club</Link>
         </Button>
         <Divider orientation="vertical" height={7} />
-        <Button size="sm" variant="ghost" rounded="sm">
+        <Button size="sm" variant="ghost" rounded="sm" _hover={{ bgColor: '#9681EB', color: 'white', rounded: 'sm' }}>
           <Link href="/score">Score</Link>
         </Button>
         <Divider orientation="vertical" height={7} />
-        <Button size="sm" variant="ghost" rounded="sm">
+        <Button size="sm" variant="ghost" rounded="sm" _hover={{ bgColor: '#9681EB', color: 'white', rounded: 'sm' }}>
           <Link href="/classement">Classement</Link>
         </Button>
       </HStack>
